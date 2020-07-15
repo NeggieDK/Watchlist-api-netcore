d9f24fb3ff256fcd842cf8d25bcba05168c9a975
Neggie <aaron.dekoninck@gmail.com>
2020-07-15 16:35:07 +0200

    Added QueryPerformanceDecorator

---
+++ WatchList-api.Test/UnitTests/DependencyInjection/DecoratorTests.cs
@@ -0,0 +1,22 @@
+﻿using LightInject;
+using WatchList_api.CQRS;
+using WatchList_api.CQRS.ActiveWatchItems.Queries.GetActiveWatchItem;
+using WatchList_api.CQRS.Interfaces;
+using Xunit;
+
+namespace WatchList_api.Test.UnitTests.DependencyInjection
+{
+    public class DecoratorTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var container = new ServiceContainer();
+            container.Register<IQuery<GetActiveWatchItemRequest, GetActiveWatchItemResponse>, GetActiveWatchItemQuery>();
+            container.Decorate<IQuery<GetActiveWatchItemRequest, GetActiveWatchItemResponse>, QueryPerformanceDecorator<GetActiveWatchItemRequest, GetActiveWatchItemResponse>>();
+
+            var instance = container.GetInstance<IQuery<GetActiveWatchItemRequest, GetActiveWatchItemResponse>>();
+            Assert.Equal(typeof(QueryPerformanceDecorator<GetActiveWatchItemRequest, GetActiveWatchItemResponse>), instance.GetType());
+        }
+    }
+}
---
+++ WatchList-api/CQRS/QueryPerformanceDecorator.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using WatchList_api.CQRS.Interfaces;
+
+namespace WatchList_api.CQRS
+{
+    public class QueryPerformanceDecorator<TRequest, TResponse> : IQuery<TRequest, TResponse>
+    {
+        private readonly IQuery<TRequest, TResponse> _baseQuery;
+        public QueryPerformanceDecorator(IQuery<TRequest, TResponse> baseQuery)
+        {
+            _baseQuery = baseQuery;
+        }
+        public TResponse Execute(TRequest request)
+        {
+            var sw1 = Stopwatch.StartNew();
+            var result = _baseQuery.Execute(request);
+            sw1.Stop();
+            Debug.WriteLine($"Executing query took {sw1.ElapsedMilliseconds}ms");
+            return result;
+        }
+    }
+}
